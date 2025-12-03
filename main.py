33e2a892c1b9eb63c9584323303fef32855292ee
Naveen Kumar <162453304+Devil-nkp@users.noreply.github.com>
2025-12-03 09:21:33 +0530

    main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("NewsWeave-v22")
 
 # API Key Strategy
-INTERNAL_API_KEY = os.environ.get("GROQ_API_KEY", "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")
+INTERNAL_API_KEY = os.environ.get("GROQ_API_KEY")
 
 app = FastAPI()
 
@@ -228,3 +228,4 @@ async def like_endpoint():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
